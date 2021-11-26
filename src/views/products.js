956b40e98941e6731ebc87b621a8a64a033c3aa3
Víctor Campos <portadict@hotmail.com>
2021-11-26 01:15:07 +0100

    minor changes

--- src/views/products.js
+++ src/views/products.js
@@ -1,3 +1,4 @@
+import "./products.scss"
 import { equalTo, get, onChildAdded, onChildRemoved, orderByChild, push, query, ref, remove, set } from '@firebase/database'
 import { useEffect, useState } from 'react'
 import { db } from '../lib/firebase'
@@ -9,13 +10,16 @@ export function Products() {
 
   const [batchProductsJson, setBatchProductsJson] = useState([])
   const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     onChildAdded(query(productsRef), productListSnap => {
       const newProduct = {
         id: productListSnap.key,
         ...productListSnap.val(),
       }
+      setLoading(false)
       setProducts(existingProducts => [newProduct, ...existingProducts])
     })
     onChildRemoved(query(productsRef), productListSnap => {
@@ -58,15 +62,25 @@ export function Products() {
   return (
     <div className="productsPage">
       <h1 className="heading">Productos</h1>
-      <h2 className="subheading">Crear productos en masa</h2>
+      <h2 className="subheading">Scripts</h2>
+      <h3 className="subsubheading">Pccomponentes</h3>
       <code>{`JSON.stringify([...document.querySelectorAll('.product-card')].map(pc => ({name: pc.parentNode.dataset.name, url: pc.parentNode.getAttribute('href'), site: 'pccomponentes' })))`}</code>
-      <textarea onChange={e => setBatchProductsJson(e.target.value)} value={batchProductsJson}></textarea>
-      <button className="blue" type="button" onClick={() => createAllProducts()}>Crear todos</button>
-      <h2 className="subheading">Crear producto</h2>
-      <Product createProduct={createProduct} />
+      <h3 className="subsubheading">Coolmod</h3>
+      <code>{`JSON.stringify([...document.querySelectorAll('[data-role=result]')].map(pc => ({name: pc.querySelector('.df-card__title').innerText, url: pc.querySelector('.df-card__main').getAttribute('href'), site: 'coolmod' })))`}</code>
+      <div className="productCreation">
+        <div className="massCreation">
+          <h2 className="subheading">Crear productos en masa</h2>
+          <textarea rows="10" onChange={e => setBatchProductsJson(e.target.value)} value={batchProductsJson}></textarea>
+          <button className="blue" type="button" onClick={() => createAllProducts()}>Crear todos</button>
+        </div>
+        <div className="individualCreation">
+          <h2 className="subheading">Crear producto</h2>
+          <Product createProduct={createProduct} />
+        </div>
+      </div>
       <h2 className="subheading">Productos actuales</h2>
       <div className="productList">
-        {products.map(p => <Product productData={p} editProduct={editProduct} deleteProduct={deleteProduct} key={p.id} />)}
+        {loading ? 'Recuperando datos...' : products.map(p => <Product productData={p} editProduct={editProduct} deleteProduct={deleteProduct} key={p.id} />)}
       </div>
     </div>
   )
